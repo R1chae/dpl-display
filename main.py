380d2c5b572912c59252be6c5103bdc916e2847b
R1chae <inbox@r1chae.com>
2023-06-13 22:10:17 +0200

    initial commit: - inserted API call - Implemented rudimentary config

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def doAPICall():
+    url = "https://dpl.easypensum.com/api/filter"
+    credentials = config['CREDENTIALS']
+
+    payload = "{\"name\":\"" + credentials['name'] + "\",\"user\":\"" + credentials['user'] + "\",\"password\":\"" + credentials['password'] + "\"}"
+    headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
+      'Accept': '*/*',
+      'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Content-Type': 'application/json; charset=utf-8',
+      'Referer': 'https://dpl.easypensum.com/',
+      'Origin': 'https://dpl.easypensum.com',
+      'DNT': '1',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-origin',
+      'Connection': 'keep-alive'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return(response)
+
+print(doAPICall().text)
